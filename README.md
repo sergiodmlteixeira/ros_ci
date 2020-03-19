40b99152246c2808ec1d8e036c0fb0ac8ac450bb
Pedro Melo <pedro.m.melo@inesctec.pt>
2020-03-19 13:00:12 +0000

    Added very very basic test suite.

---
+++ README.md
@@ -0,0 +1,2 @@
+# ROS_CI Package
+A simple ROS package for continuous integration (CI) tests. Developed in Python.
---
+++ test/test_ci.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import rostest
+import sys
+import unittest
+
+PKG = 'ros_ci'
+
+class SampleTest(unittest.TestCase):
+ 
+    def test_one_equals_one(self):
+        self.assertEquals(1, 1, "1 != 1")
+
+# test entry point
+if __name__ == '__main__':
+
+    rostest.rosrun(PKG, 'sample_test', SampleTest)
