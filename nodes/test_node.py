be4cdd5fe226d391d21bbb1abfc10da30570ca72
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-06 16:14:24 +0000

    Adding several problems

--- nodes/test_node.py
+++ nodes/test_node.py
@@ -9,6 +9,10 @@ if __name__ == "__main__":
     rospy.init_node('test_node')
 
     try:
+        a = 2
+        b = 3
+        a = a
+        b = 3
         rospy.loginfo('Test node!')
         rospy.spin()
 
