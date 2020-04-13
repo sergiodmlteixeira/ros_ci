3f25dbdac11309e2e430b9c63050ecb8f7fc92ad
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-13 18:15:26 +0000

    Fix

--- after_script.sh
+++ after_script.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-python -m coverage xml /root/target_ws/build/ros_ci/ros_ci_python/.coverage
+cd /root/target_ws/build/ros_ci/ros_ci_python/
+python -m coverage xml 
 
 curl -Ls -o codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
 chmod +x codacy-coverage-reporter
 ./codacy-coverage-reporter report -l Python -r /root/target_ws/build/ros_ci/ros_ci_python/coverage.xml
 
 # find -type f -name .coverage -printf "%h\n" 
+# for i in $(find /home/rarrais/catkin_ws -type f -name .coverage); 
+# do
+#     echo "$i"
+# done
