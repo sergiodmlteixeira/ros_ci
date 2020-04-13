8b7c7db425d126143c5f9bf0773bac7d9f1f4bf4
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-13 15:54:59 +0000

    Adding Codacy token env and fixing bash script

--- after_script.sh
+++ after_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd /root/target_ws/build/ros_ci/ros_ci_python/ && python -m coverage xml 
-cd /root/target_ws/build/ros_ci/ros_ci_python/ && wget -qO - https://coverage.codacy.com/get.sh | sh -s report -l Python -r coverage.xml
+cd /root/target_ws/build/ros_ci/ros_ci_python/ 
+python -m coverage xml 
+wget -qO - https://coverage.codacy.com/get.sh | sh -s report -l Python -r coverage.xml
 
 # find -type f -name .coverage -printf "%h\n" 
