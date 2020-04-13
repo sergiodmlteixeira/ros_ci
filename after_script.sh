95fe77f43bee4900d37544bf17dda863312910f4
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-13 17:59:07 +0000

    Adding curl as a dependency

--- after_script.sh
+++ after_script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cd /root/target_ws/build/ros_ci/ros_ci_python/ 
-python -m coverage xml 
-wget -qO - https://coverage.codacy.com/get.sh | sh -s report -l Python -r coverage.xml
+python -m coverage xml /root/target_ws/build/ros_ci/ros_ci_python/ 
+
+curl -Ls -o codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
+chmod +x codacy-coverage-reporter
+./codacy-coverage-reporter report -l Python -r /root/target_ws/build/ros_ci/ros_ci_python/coverage.xml
 
 # find -type f -name .coverage -printf "%h\n" 
