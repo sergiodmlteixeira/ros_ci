040262ef54b3b5ae49f937d798aeb23ce570ba01
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-13 18:08:19 +0000

    Adding missing dependencies

--- after_script.sh
+++ after_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python -m coverage xml /root/target_ws/build/ros_ci/ros_ci_python/ 
+python -m coverage xml /root/target_ws/build/ros_ci/ros_ci_python/.coverage
 
 curl -Ls -o codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
 chmod +x codacy-coverage-reporter
