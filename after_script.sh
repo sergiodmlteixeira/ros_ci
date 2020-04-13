56eca354330cbf2a32222a163be867e7eab44f8f
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-13 18:30:02 +0000

    Changing order or params?

--- after_script.sh
+++ after_script.sh
@@ -6,7 +6,7 @@ echo $TRAVIS_COMMIT
 
 curl -Ls -o codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
 chmod +x codacy-coverage-reporter
-./codacy-coverage-reporter report -l Python -r /root/target_ws/build/ros_ci/ros_ci_python/coverage.xml --commit-uuid $TRAVIS_COMMIT
+./codacy-coverage-reporter report --commit-uuid $TRAVIS_COMMIT -l Python -r /root/target_ws/build/ros_ci/ros_ci_python/coverage.xml 
 
 # find -type f -name .coverage -printf "%h\n" 
 # for i in $(find /home/rarrais/catkin_ws -type f -name .coverage); 
