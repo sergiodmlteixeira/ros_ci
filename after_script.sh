e77cddec47672f683e195f9b4538914d4af9846a
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-14 10:55:36 +0000

    Testing permissions and new env structure

--- after_script.sh
+++ after_script.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# Combine .coverage files & Combine result to xml
 python -m coverage combine `find /root/target_ws -type f -name .coverage`
-python -m coverage xml
+# python -m coverage xml
 
-# Setup & Upload Codacy Reporter
-curl -Ls -o codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
-chmod +x codacy-coverage-reporter
-./codacy-coverage-reporter report --commit-uuid $TRAVIS_COMMIT -l Python -r coverage.xml
+# # Setup & Upload Codacy Reporter
+# curl -Ls -o codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
+# chmod +x codacy-coverage-reporter
+# ./codacy-coverage-reporter report --commit-uuid $TRAVIS_COMMIT -l Python -r coverage.xml
