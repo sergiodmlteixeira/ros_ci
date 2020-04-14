1621cb4d64d416be020d06821eb8f74966f5c053
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-14 15:53:32 +0000

    Debug

--- codacy.sh
+++ codacy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# cd to a directory with write permissions
-cd /root/target_ws/
+# # cd to a directory with write permissions
+# cd /root/target_ws/
 
-# Combine .coverage files & Combine result to xml
-python -m coverage combine `find . -type f -name .coverage`
-python -m coverage xml
+# # Combine .coverage files & Combine result to xml
+# python -m coverage combine `find . -type f -name .coverage`
+# python -m coverage xml
 
 # Generate Cpp coverage xml
 
@@ -15,17 +15,17 @@ echo "${GCDA}"
 GCNO="$(find /root/target_ws/ -type f -name *.gcno)"
 echo "${GCNO}"
 
-OUTPUT="$(gcovr -v -r /root/target_ws/)"
-echo "${OUTPUT}"
+# OUTPUT="$(gcovr -v -r /root/target_ws/)"
+# echo "${OUTPUT}"
 
 # gcovr -r /root/target_ws/ --xml-pretty > coverage_cpp.xml
 
 # Setup & Upload Codacy Reporter
-curl -Ls -o codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
-chmod +x codacy-coverage-reporter
+# curl -Ls -o codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
+# chmod +x codacy-coverage-reporter
 
 # Report Python coverage
 # ./codacy-coverage-reporter report --commit-uuid $TRAVIS_COMMIT -l Python -r coverage.xml
 
 # Report Cpp coverage
-./codacy-coverage-reporter report --commit-uuid $TRAVIS_COMMIT --language Cpp --force-language -r coverage_cpp.xml
+# ./codacy-coverage-reporter report --commit-uuid $TRAVIS_COMMIT --language Cpp --force-language -r coverage_cpp.xml
