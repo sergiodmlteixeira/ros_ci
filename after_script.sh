79b34de8ebd210ee89638872b789fce1aca53111
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-14 10:11:33 +0000

    New attempt at after_script

--- after_script.sh
+++ after_script.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-
-# find -type f -name .coverage -printf "%h\n" 
-for i in $(find /home/rarrais/catkin_ws -type f -name .coverage); 
-do
-    echo "$i"
-done
-
-cd /root/target_ws/build/ros_ci/ros_ci_python/
 # Convert .coverage to xml file
-python -m coverage xml 
-
+for i in $(find /root/target_ws -type f -name .coverage); 
+do
+  cd $(dirname $(realpath $i)) && python -m coverage xml
+done
 
 # Setup Codacy Reporter
 curl -Ls -o codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
 chmod +x codacy-coverage-reporter
 
 # Upload coverage xml to Codacy
-./codacy-coverage-reporter report --commit-uuid $TRAVIS_COMMIT -l Python -r /root/target_ws/build/ros_ci/ros_ci_python/coverage.xml 
-
+for i in $(find /root/target_ws -type f -name coverage.xml); 
+do
+  ./codacy-coverage-reporter report --commit-uuid $TRAVIS_COMMIT -l Python -r $i 
+done
