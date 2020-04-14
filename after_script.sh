7555abbcd8b026c8deec5415bb13a1344af84ccb
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-14 11:17:30 +0000

    Testing with permissions issue solved

--- after_script.sh
+++ after_script.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Combine .coverage files & Combine result to xml
+# cd to a directory with write permissions
 cd /root/target_ws/
-python -m coverage combine `find . -type f -name .coverage`
-# python -m coverage xml
 
-# # Setup & Upload Codacy Reporter
-# curl -Ls -o codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
-# chmod +x codacy-coverage-reporter
-# ./codacy-coverage-reporter report --commit-uuid $TRAVIS_COMMIT -l Python -r coverage.xml
+# Combine .coverage files & Combine result to xml
+python -m coverage combine `find . -type f -name .coverage`
+python -m coverage xml
+
+# Setup & Upload Codacy Reporter
+curl -Ls -o codacy-coverage-reporter "$(curl -Ls https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | contains("codacy-coverage-reporter-linux"))) | .[0].browser_download_url')"
+chmod +x codacy-coverage-reporter
+./codacy-coverage-reporter report --commit-uuid $TRAVIS_COMMIT -l Python -r coverage.xml
