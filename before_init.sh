ebaa44e16961c10caec1fd3b0b5f6fb30bdcba8e
Rafael Arrais <rafael.arrais@gmail.com>
2020-04-15 17:26:40 +0000

    New travis.yml structure and removing files not needed

--- before_init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# apt-get install jq
-curl -LSs "$(curl -LSs https://api.github.com/repos/codacy/codacy-coverage-reporter/releases/latest | jq -r '.assets | map({name, browser_download_url} | select(.name | endswith(".jar"))) | .[0].browser_download_url')" -o codacy-coverage-reporter-assembly.jar
